6b1cfb380b8fa32d5dc1c9d6533462978db55248
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-24 03:33:48 -0500

    userId pulled from params

--- src/user/User.js
+++ src/user/User.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import UserProfile from "./UserProfile";
 import { fetchUserWithPosts } from "../api";
 import PostList from "./PostList";
@@ -8,7 +9,7 @@ import ErrorMessage from "../common/ErrorMessage";
 export const User = () => {
   const [user, setUser] = useState({ posts: [] });
   const [error, setError] = useState(undefined);
-  const userId = 1; // TODO: This ID will need to be pulled from parameters.
+  const { userId } = useParams(); // (x)TODO: This ID will need to be pulled from parameters.
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -25,7 +26,7 @@ export const User = () => {
     return (
       <ErrorMessage error={error}>
         <p>
-          <a>Return Home</a>
+          <Link to={`/`}>Return Home</Link>
         </p>
       </ErrorMessage>
     );
